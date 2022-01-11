4bffe98a3774799c0962df25a2d5359dd86af5fb
revisionprojects <robinmatth@live.ca>
2022-01-11 10:47:58 -0500

    configured local settings for postgres

--- django_boilerplate/config/settings/base.py
+++ django_boilerplate/config/settings/base.py
@@ -42,7 +42,7 @@ LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    "default": env.db("DATABASE_URL", default="postgres://localhost/django_boilerplate"),
+    "default": env.db("DATABASE_URL", default="postgres://postgres:admin@127.0.0.1:5432/django_boilerplate"),
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
